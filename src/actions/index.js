3a2576772aaf186d53008569c98b3842ae45716b
Min Khati <meetup.min@gmail.com>
2018-03-12 09:42:01 -0400

    Delete a post and return to the root url from show post component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST  = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
+
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=minunique123';
@@ -33,3 +35,15 @@ export function fetchPost(id) {
     payload: request
   };
 }
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
+    payload: request
+  };
+}
+
+
+
